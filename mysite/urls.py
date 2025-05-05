620e49534b1f350412337a0ab0dd36af95c0d528
Anderson Costa <costaandersom23@gmail.com>
2025-05-05 16:39:27 -0300

    finalisado modulo views

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('blog.urls')),
+    path('', index, name='index'),
 ]
